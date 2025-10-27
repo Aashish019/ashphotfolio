f4dd2384a2d05bd2986750d35ef511c09c9125d6
Aashish <aashishanil530@gmail.com>
2025-10-27 18:51:35 +0530

    Updates

---
+++ _next/static/cDSAiR3tvfYsmF1Sjk3_1/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ashphotfolio//_next/:path+",
+        "destination": "/ashphotfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
